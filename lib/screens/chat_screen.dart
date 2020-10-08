71b03c398e3991548d3c21236511aff21330bf38
igorvidottof <igor_vidotto@hotmail.com>
2020-10-08 16:12:06 -0300

    lesson 316 - Adding Data

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -29,7 +29,11 @@ class ChatScreen extends StatelessWidget {
           }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          Firestore.instance
+              .collection('/chats/4zTCC33NBwtmFCVx1AWW/messages')
+              .add({'text': 'Another Hello World'});
+        },
       ),
     );
   }
