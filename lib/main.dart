f393885ec680fce9793d720fd93657ffbbb59d76
igorvidottof <igor_vidotto@hotmail.com>
2020-10-08 16:32:04 -0300

    lesson 317 - A Basic Authentication Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_module/screens/chat_screen.dart';
+import 'package:firebase_module/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_module/widgets/auth/auth_form.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AuthForm(),
+    );
+  }
+}
