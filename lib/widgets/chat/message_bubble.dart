b134ea6a644173616913c5e355670fc8f2f7460e
igorvidottof <igor_vidotto@hotmail.com>
2020-10-13 18:43:35 -0300

    lesson 329 - Improving the Chat Bubbles

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -2,25 +2,36 @@ import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   final String message;
+  final bool isMe;
+  // CREATE A KEY TO ENSURE THAT FLUTTER WON'T MESS UP THE MESSAGES
+  final Key key;
 
-  MessageBubble(this.message);
+  MessageBubble(this.message, this.isMe, {this.key});
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           width: 140,
           decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.circular(12),
+            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+              bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+            ),
           ),
           padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
           margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
           child: Text(
             message,
             style: TextStyle(
-              color: Theme.of(context).accentTextTheme.headline6.color,
+              color: isMe
+                  ? Colors.black
+                  : Theme.of(context).accentTextTheme.headline6.color,
             ),
           ),
         ),
--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,31 +1,43 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_module/widgets/chat/message_bubble.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance
-          .collection('chat')
-          .orderBy('createdAt', descending: true)
-          .snapshots(),
-      builder: (context, chatSnapshot) {
-        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (ctx, futureSnapshot) {
+        if (futureSnapshot.connectionState == ConnectionState.waiting) {
           return Center(
             child: CircularProgressIndicator(),
           );
         }
-        final querySnapshot = chatSnapshot.data as QuerySnapshot;
-        final documents = querySnapshot.documents;
-        return ListView.builder(
-          // REVERSE MAKES THE LISTVIEW SCROLL FROM BOTTOM TO TOP
-          reverse: true,
-          itemCount: documents.length,
-          // itemBuilder: (ctx, index) => Text(
-          //   documents[index]['text'],
-          // ),
-          itemBuilder: (ctx, index) => MessageBubble(documents[index]['text']),
+        return StreamBuilder(
+          stream: Firestore.instance
+              .collection('chat')
+              .orderBy('createdAt', descending: true)
+              .snapshots(),
+          builder: (context, chatSnapshot) {
+            if (chatSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final querySnapshot = chatSnapshot.data as QuerySnapshot;
+            final documents = querySnapshot.documents;
+            return ListView.builder(
+              // REVERSE MAKES THE LISTVIEW SCROLL FROM BOTTOM TO TOP
+              reverse: true,
+              itemCount: documents.length,
+              itemBuilder: (ctx, index) => MessageBubble(
+                documents[index]['text'],
+                documents[index]['userId'] == futureSnapshot.data.uid,
+                key: ValueKey(documents[index].documentID),
+              ),
+            );
+          },
         );
       },
     );
