41163c1435a85752961d1e1295a4d59975126caf
igorvidottof <igor_vidotto@hotmail.com>
2020-10-20 10:37:31 -0300

    lesson 346 - Setting a Firestore Trigger

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,8 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// LISTENS TO CHANGES ON THE CHAT COLLECTION, ON ANY DOCUMENTS
+exports.myFunction = functions.firestore
+  .document('chat/{message}')
+  .onCreate((snapshot, context) => {
+      console.log(snapshot.data());
+  });
